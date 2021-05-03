30ac57527e8124dcd3debb59d5b7b769232d401c
Alejandro Salguero <alejandrosalgueroq@hotmail.com>
2021-05-03 17:54:27 -0600

    Archivos sass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles/styles.scss"; //npm install node-sass, para ver los estilos
 import { JournalApp } from "./JournalApp";
 
 ReactDOM.render(<JournalApp />, document.getElementById("root"));

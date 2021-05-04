f2515fd7df2a79576b133437963ae21ea9e6a87b
Alejandro Salguero <alejandrosalgueroq@hotmail.com>
2021-05-03 21:21:55 -0600

    Estilos login

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,9 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const LoginScreen = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <h3 className="auth__tittle">Login</h3>
+      <form>
+        <input
+          className="auth__input"
+          type="text"
+          placeholder="Email"
+          name="email"
+          autoComplete="off"
+        />
+        <input
+          className="auth__input"
+          type="Password"
+          placeholder="password"
+          name="password"
+        />
+        <button className="btn btn-primary btn-block" type="submit">
+          Login
+        </button>
+
+        <div className="auth__social-networks">
+          <p>Login with social networks</p>
+        </div>
+        <div className="google-btn">
+          <div className="google-icon-wrapper">
+            <img
+              className="google-icon"
+              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+              alt="google button"
+            />
+          </div>
+          <p className="btn-text">
+            <b>Sign in with google</b>
+          </p>
+        </div>
+        <br />
+        <Link to="/auth/register">Create account</Link>
+      </form>
+    </>
   );
 };
